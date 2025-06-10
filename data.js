c68ef002a5d4ebb6620fae08c2058359c978c3a3
E-RKP <edobackup111@gmail.com>
2025-06-10 11:39:58 +0200

    version 3

--- data.js
+++ data.js
@@ -9,3 +9,41 @@ const libro = request.response
 console.log(libro)
 }
 
+const numerino = document.getElementById('ok')
+const numerone = document.getElementById('numerone')
+const buttondimi = document.getElementById('dimi')
+const buttonagg = document.getElementById('agg')
+let counterm = 0
+let counterBIG = 0
+
+
+
+
+function diminuisci(){
+  counterm++;
+  counterBIG--;
+
+  if (counterBIG<0){
+    counterBIG=0
+  }
+
+  return{
+    counterBIG,
+    counterm
+  }
+}
+
+buttondimi.addEventListener('click', ()=>{
+  diminuisci();
+  numerino.innerHTML = counterm,
+  numerone.innerHTML = counterBIG
+})
+
+buttonagg.onclick = function(){
+  counterBIG++;
+  counterm++;
+
+  numerino.innerHTML = counterm
+  numerone.innerHTML = counterBIG
+}
+
